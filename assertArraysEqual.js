933633279a307838a1e4756a7053129ee6cb059c
IrinaGM <irina.golionko@gmail.com>
2023-03-26 03:02:49 +0000

    Added function of assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+// function that takes two arrays and returns boolean based on a perfect match
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+//Function that recives two arrays as arguments and console.log an appropriate message to the console.
+const assertEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual([1, 2, 3], [1, 2, 3]);
+assertEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertEqual(["1", "2", "3"], ["1", "2", 3]);
+assertEqual([], []);
+assertEqual([], [1]);
+assertEqual("abc", "abc");
