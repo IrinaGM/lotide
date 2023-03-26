4f663f07deff2b4cdd79aee98b0d9219faf30e6b
IrinaGM <irina.golionko@gmail.com>
2023-03-26 02:31:43 +0000

    Added eqArrays,js function

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// function that takes two arrays and returns boolean based on a perfect match
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays([], [1]), false);
+assertEqual(eqArrays("abc", "abc"), true);
