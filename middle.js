af48dfe8d7eeec9521430fa3e3bc008379a38fbe
IrinaGM <irina.golionko@gmail.com>
2023-03-27 00:46:46 +0000

    added addional tests

--- middle.js
+++ middle.js
@@ -40,6 +40,12 @@ const middle = function (array) {
   return middle;
 };
 
+//test to see that middle function does not change the original array
+const testArray = [1, 2, 3, 4];
+middle(testArray);
+assertArraysEqual(testArray, [1, 2, 3, 4]);
+
+// additional tests
 assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
