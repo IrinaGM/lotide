7c8664a0ba3120a532e7b9c1561c51ad9ff1b11f
IrinaGM <irina.golionko@gmail.com>
2023-03-31 03:08:09 +0000

    Implemented assertObjectsEqual.js function

---
+++ assertObjectsEqual.js
@@ -0,0 +1,67 @@
+/*
+ * function that takes two arrays and returns boolean based on a perfect match
+ */
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+/*
+ * Function that takes in two Objects.
+ * Returns true if both objects have identical keys with identical values.
+ * Otherwise returns false.
+ */
+
+const eqObjects = function (obj1, obj2) {
+  if (Object.keys(obj1).length === Object.keys(obj2).length) {
+    for (const key in obj1) {
+      // check if object - TODO
+
+      // check if array
+      if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
+        let returnedAnswer = eqArrays(obj1[key], obj2[key]);
+        if (returnedAnswer === false) {
+          return false;
+        }
+      } else {
+        // else compare prim values
+        if (obj1[key] !== obj2[key]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require("util").inspect;
+  if (eqObjects(actual, expected)) {
+    return console.log(
+      `✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
+    );
+  } else {
+    return console.log(
+      `🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`
+    );
+  }
+};
+
+/*
+ * ---- Test scenarios ----
+ */
+
+assertObjectsEqual({ 1: "test" }, { 1: "test" });
+assertObjectsEqual({ 1: 0 }, { 2: 0 });
+assertObjectsEqual({}, {});
+assertObjectsEqual({ 1: ["Test", 2] }, { 1: ["Test", 2] });
