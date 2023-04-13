36676a7ba7e283ae28071869c3c5bcbd834b8832
IrinaGM <irina.golionko@gmail.com>
2023-04-13 02:35:49 +0000

    Refactored assertArraysEqual.js to export function. Moved testing code into test/assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-// function that takes two arrays and returns boolean based on a perfect match
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 //Function that recives two arrays as arguments and console.log an appropriate message to the console.
 const assertArraysEqual = function (actual, expected) {
@@ -21,9 +9,4 @@ const assertArraysEqual = function (actual, expected) {
   }
 };
 
-assertEqual([1, 2, 3], [1, 2, 3]);
-assertEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertEqual(["1", "2", "3"], ["1", "2", 3]);
-assertEqual([], []);
-assertEqual([], [1]);
-assertEqual("abc", "abc");
+module.exports = assertArraysEqual;
