b30459b07347957fcc3788865ba44ebf0edec082
IrinaGM <irina.golionko@gmail.com>
2023-04-14 02:41:48 +0000

    Refactored tailTest.js to use mocha and chai tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,32 @@
 const tail = require("../tail.js");
-const assertEqual = require("../assertEqual.js");
+const assert = require("chai").assert;
+const expect = require("chai").expect;
 
-assertEqual(JSON.stringify(tail([5, 6, 7])), `[6,7]`);
-assertEqual(
-  JSON.stringify(tail(["Hello", "Lighthouse", "Labs"])),
-  `["Lighthouse","Labs"]`
-);
-assertEqual(tail("Test"), `est`);
-assertEqual(JSON.stringify(tail(["one"])), `[]`);
-assertEqual(JSON.stringify(tail([])), `[]`);
-assertEqual(tail(), undefined);
-
-//Check that origial array remains has no side affects after tail fucntion
-const testArr = [1, 2, 3, 4];
-tail(testArr);
-assertEqual(testArr.length, 4);
+describe("#tail", () => {
+  it("returns [6,7] for [5,6,7]", () => {
+    expect(tail([5, 6, 7])).to.eql([6, 7]);
+  });
+  it("returns ['Lighthouse','Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    expect(tail(["Hello", "Lighthouse", "Labs"])).to.eql([
+      "Lighthouse",
+      "Labs",
+    ]);
+  });
+  it("returns 'est' for 'Test'", () => {
+    assert.strictEqual(tail("Test"), "est");
+  });
+  it("returns [] for ['one']", () => {
+    expect(tail(["one"])).to.eql([]);
+  });
+  it("returns [] for []", () => {
+    expect(tail([])).to.eql([]);
+  });
+  it("returns undefined for tail()", () => {
+    assert.strictEqual(tail(), undefined);
+  });
+  it("checks that original array has not been affected after tail funtion exectution", () => {
+    const testArr = [1, 2, 3, 4];
+    tail(testArr);
+    assert.strictEqual(testArr.length, 4);
+  });
+});
